13de6167e6f683d67abe89c75e089bc0e00fa1b9
Carlo Limiti <carlolimiti1@gmail.com>
2026-01-28 14:31:52 -0300

    Hicimos un contador

---
+++ wordle.py
@@ -0,0 +1,11 @@
+#Ni idea
+contador = 0
+while True:
+
+    #Funcion de contador
+    contador = contador + 1
+    if contador < 6:
+        print(f'Te quedan {6-contador} intentos')
+    else :
+        print('Perdiste')
+        break
