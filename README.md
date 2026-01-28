614d8e78c3c0c0ae76f8f352e754e4d6572cbacc
Carlo Limiti <carlolimiti1@gmail.com>
2026-01-28 15:28:40 -0300

    Trabajo en progreso

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wordle
-#   Debi nacer antes
+    Primero realizamos un contador, porque se me hizo mas sencillo
+
+    Creamos los escenarios en los que la palabra ingresada sea = a la ganadora
+y en caso de que se acabaran los intentos.
+
+    Ahora estamos desarrollando el comparador de letras entre las 2 palabras para integrarlo en un elif
--- wordle.py
+++ wordle.py
@@ -1,19 +1,28 @@
-#Ni idea
-palabra_ganadora = 'pollo'
-contador = 0
-while True:
-    adivinanza = input('Ingrese palabra de 5 letras')
-    if palabra_ganadora == adivinanza : #Escenario de Acierto
-            print('Has acertado')
-            break
-    else : #Intentos fallidos
-    #Funcion de contador, introducir dentro de los intentos fallidos
-               contador = contador + 1
-    if contador < 6:
-        print(f'Te quedan {6-contador} intentos')
-    else :
-        print('Perdiste')
-        break
+# #Ni idea
+# palabra_ganadora = 'pollo'
+# contador = 0
+# while True:
+#     adivinanza = input('Ingrese palabra de 5 letras')
+#     if palabra_ganadora == adivinanza : #Escenario de Acierto
+#             print('Has acertado')
+#             break
+#     elif
+#     else : #Intentos fallidos
+#     #Funcion de contador, introducir dentro de los intentos fallidos
+#                contador = contador + 1
+#     if contador < 6:
+#         print(f'Te quedan {6-contador} intentos')
+#     else :
+#         print('Perdiste')
+#         break
 
-    
- 
+adivinanza = input('Palabra de 5 letras')
+intentos = list(adivinanza)
+palabra_ganadora = 'pollo'
+lista_ganadora = list(palabra_ganadora)
+print(lista_ganadora)
+
+for letras in range(5):
+    if intentos[letras] == lista_ganadora:
+        print(f'Tiene {letra}')
+
