79bd24c734b72fd0adc54b9ff76d8329fd52fb09
Carlo Limiti <carlolimiti1@gmail.com>
2026-01-28 14:49:50 -0300

    Creamos los escenarios de acierto y fallos, unimos el contador a los fallos

--- wordle.py
+++ wordle.py
@@ -1,11 +1,19 @@
 #Ni idea
+palabra_ganadora = 'pollo'
 contador = 0
 while True:
-
-    #Funcion de contador
-    contador = contador + 1
+    adivinanza = input('Ingrese palabra de 5 letras')
+    if palabra_ganadora == adivinanza : #Escenario de Acierto
+            print('Has acertado')
+            break
+    else : #Intentos fallidos
+    #Funcion de contador, introducir dentro de los intentos fallidos
+               contador = contador + 1
     if contador < 6:
         print(f'Te quedan {6-contador} intentos')
     else :
         print('Perdiste')
         break
+
+    
+ 
